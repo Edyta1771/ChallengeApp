c71a42cfca7a55140be0509b50147387ecf39669
Edyta1771 <emadej3@aim.com>
2023-02-20 23:36:10 +0100

    Zadanie domowe Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello Edyta!");
+﻿var name = "Ewa";
+bool female = true;
+var age = 27;
+
+if (female && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
